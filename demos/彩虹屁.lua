7e2c816a48334de905b0c0b135bcb5c5d2f2a593
ONLY-LONELY-520 <65522568+ONLY-LONELY-520@users.noreply.github.com>
2020-09-20 11:19:03 +0000

    Update 彩虹屁.lua
    000

--- demos/彩虹屁.lua
+++ demos/彩虹屁.lua
@@ -3,7 +3,7 @@ Info.name="彩虹屁"
 Info.author="ooooonly"
 Info.version="1.0"
 Info.description="试着向机器人发送‘夸我’"
-
+--Hi
 --[[ 单独运行时启用此代码
     local bot = Bot(123456,"abcderf","device.json") -- 替换为自己的账号密码
     bot:login()
